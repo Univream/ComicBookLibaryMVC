1c30ff7137957868f4d5c63ef91a0ad81dc08a97
Hakim Rachidi <Hakimrachidi02@gmail.com>
2018-04-09 07:04:11 +0200

    Defined a generic BaseRepository for Entities

---
+++ src/ComicBookLibrary/ComicBookShared/Data/BaseRepository.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComicBookShared.Data
+{
+    public abstract class BaseRepository<TEntity> 
+        where TEntity : class
+    {
+        protected Context Context { get; private set; }
+
+        public BaseRepository()
+        {
+            Context = new Context();
+        }
+
+
+        public abstract TEntity Get(int id);
+        public abstract IList<TEntity> GetList();
+
+        public void Add(TEntity entity)
+        {
+            Context.Set<TEntity>().Add(entity);
+            Context.SaveChanges();
+        }
+
+        public void Update(TEntity entity)
+        {
+            Context.Entry(entity).State = EntityState.Modified;
+            Context.SaveChanges();
+        }
+
+        public void Delete(int id)
+        {
+            TEntity entity = Context.Set<TEntity>().Find(id);
+            Context.Set<TEntity>().Remove(entity);
+            Context.SaveChanges();
+        }
+    }
+}
